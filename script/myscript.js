aa44e6648be4a4a60d156073c68310e0fbbe6869
Nawa Acharya <git@gitlab.com:nawa.acharya04/openMart.git>
2016-10-24 00:46:23 -0500

    Google Map API integrated and provided Fairfield location with Marker Interface

--- script/myscript.js
+++ script/myscript.js
@@ -1,5 +1,7 @@
 "use strict";
 $(document).ready(function() {
+	myLocation();
+	
     $("#contactForm").hide();
     $("#messageSend").hover(function(e) {
        $("#contactForm").fadeIn("slow")
@@ -10,7 +12,23 @@ $(document).ready(function() {
 		$("#contactForm").fadeOut();
 	});
 	
-	$(window).scroll(function() {
-		
+
+	$("#educate").mouseenter(function() {
+		$("#educate").effect('slide', 'right');
 	});
-});
+	
+	function myLocation() {
+		  var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 14,
+			center: new google.maps.LatLng(41.0177, -91.9672), 
+		  });
+
+		  var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(41.0177, -91.9672), 
+			map: map,
+			title: 'Nawa Location'
+		  });
+		marker.setMap(map);
+	}
+
+})();

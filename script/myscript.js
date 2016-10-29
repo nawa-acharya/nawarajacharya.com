1b622cfe8c06a54a6637a95d6ee3cb7113a1d229
Nawa Acharya <git@gitlab.com:nawa.acharya04/openMart.git>
2016-10-29 13:59:16 -0500

    Extra margin on right removed

--- script/myscript.js
+++ script/myscript.js
@@ -1,25 +1,24 @@
 "use strict";
 $(document).ready(function() {
+	
 	myLocation();
+	
+	var $educationHeader = $('#educate');
+	var $windowHeight = $(window).height();
+	var $docHeight = $(document).height();
 	
     $("#contactForm").hide();
     $("#messageSend").hover(function(e) {
-       $("#contactForm").fadeIn("slow")
-	   .addClass("bg-5");
+       $("#contactForm").fadeIn("slow").addClass("bg-5");
 	});
 	
 	$("#contactme").mouseleave(function() {
 		$("#contactForm").fadeOut();
 	});
 	
-
-	$("#educate").mouseenter(function() {
-		$("#educate").effect('slide', 'right');
-	});
-	
 	function myLocation() {
 		  var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 14,
+			zoom: 12,
 			center: new google.maps.LatLng(41.0177, -91.9672), 
 		  });
 
@@ -29,6 +28,20 @@ $(document).ready(function() {
 			title: 'Nawa Location'
 		  });
 		marker.setMap(map);
-	}
+	};
+
+	$(window).scroll(function() {
+		/*animateMyPage();*/
+	});
+/*	
+	function animateMyPage() {
+		var windowTop = $(window).scrollTop();
+		var topCoordinates = $educationHeader.offset().top;
+		if(windowTop > (topCoordinates - ($windowHeight*0.75)){
+			console.log("Here we go");
+			$("#educate").slide("right");	
+		};
+	};*/
+	
 
 })();

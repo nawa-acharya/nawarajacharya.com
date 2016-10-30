89d4c6c89c7228b9b8217472945cfca6ba38a13d
Nawa Acharya <git@gitlab.com:nawa.acharya04/openMart.git>
2016-10-30 13:38:24 -0500

    career note added

--- script/myscript.js
+++ script/myscript.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 	
 	myLocation();
 	
-	var $educationHeader = $('#educate');
 	var $windowHeight = $(window).height();
-	var $docHeight = $(document).height();
+	var $docHeight = $(document).height();	
 	
+	//show hide message field on hover
     $("#contactForm").hide();
     $("#messageSend").hover(function(e) {
        $("#contactForm").fadeIn("slow").addClass("bg-5");
@@ -16,6 +16,7 @@ $(document).ready(function() {
 		$("#contactForm").fadeOut();
 	});
 	
+	//show google map with marker
 	function myLocation() {
 		  var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 12,
@@ -30,18 +31,29 @@ $(document).ready(function() {
 		marker.setMap(map);
 	};
 
-	$(window).scroll(function() {
-		/*animateMyPage();*/
-	});
-/*	
-	function animateMyPage() {
-		var windowTop = $(window).scrollTop();
-		var topCoordinates = $educationHeader.offset().top;
-		if(windowTop > (topCoordinates - ($windowHeight*0.75)){
-			console.log("Here we go");
-			$("#educate").slide("right");	
-		};
-	};*/
-	
+	function animations() {
+		//for animations 
+		var $window = $(window);
+		$window.on('scroll resize', myAnimation());
+		$window.trigger('scroll');
 
+		function myAnimation() {
+		  var window_height = $window.height();
+		  var window_top_position = $window.scrollTop();
+		  var window_bottom_position = (window_top_position + window_height);
+
+			var $element = $('#careerNote');
+			var element_height = $element.outerHeight();
+			var element_top_positions = $element.offset().top;
+			var element_bottom_position = (element_top_position + element_height);
+
+			//check to see if this current container is within viewport
+			if ((element_bottom_position >= window_top_position) &&
+				(element_top_position <= window_bottom_position)) {
+			  $(this).show('slide', '{direction: "right"}', 1000);
+				console.log("Check")
+			};
+		};
+	}
+	
 })();
